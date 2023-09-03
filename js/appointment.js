ec7bc416117509d931e693ab40ec4242b70d7cc9
Yashvanth Jangam <yashvanthjangam2023@gmail.com>
2023-09-03 09:15:45 -0500

    Adding javascript for Appointments Confirmation

--- js/appointment.js
+++ js/appointment.js
@@ -1,20 +1,29 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-  
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        form.classList.add("was-validated");
+        const fistname = document.getElementById('firstName');
+        const lastName = document.getElementById('lastName');
+        const email = document.getElementById('email');
+        const phone = document.getElementById('phone');
+        console.log("Appointment Booked and Details");
+        console.log("fistname", fistname.value);
+        console.log("lastName", lastName.value);
+        console.log("email", email.value);
+        console.log("phone", phone.value); 
+       },
+      false
+    );
+  });
+})();
