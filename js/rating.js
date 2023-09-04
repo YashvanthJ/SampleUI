619031a2dd3899a8f05b2e88cf581498dd08657e
Yashvanth Jangam <yashvanthjangam2023@gmail.com>
2023-09-03 21:41:35 -0500

    Replace appointment by rating UI

--- js/rating.js
+++ js/rating.js
@@ -17,11 +17,22 @@
         const lastName = document.getElementById('lastName');
         const email = document.getElementById('email');
         const phone = document.getElementById('phone');
-        console.log("Appointment Booked and Details");
+        console.log("Rating on hair cut and Details");
         console.log("fistname", fistname.value);
         console.log("lastName", lastName.value);
         console.log("email", email.value);
         console.log("phone", phone.value);
+        if(document.getElementById('star1').checked) {
+          console.log("Hair Cut Rating is 1 star");
+        } else if(document.getElementById('star2').checked) {
+          console.log("Hair Cut Rating is 2 star");
+        } else if(document.getElementById('star3').checked) {
+          console.log("Hair Cut Rating is 3 star");
+        } else if(document.getElementById('star4').checked) {
+          console.log("Hair Cut Rating is 4 star");
+        } else if(document.getElementById('star5').checked) {
+          console.log("Hair Cut Rating is 5 star");
+        }
         document.getElementById('appointments').reset();
        },
       false
